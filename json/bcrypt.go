ca13ef569392a226c83b0579a4b82b436b6ed1a8
lucasraal <lucasraal@gmail.com>
2022-06-19 03:54:14 -0300

    go mod 2

--- json/bcrypt.go
+++ json/bcrypt.go
@@ -7,16 +7,28 @@ import (
 )
 
 func main() {
+
+	// collects "new password"
 	password := "1234"
 	byte_password := []byte(password)
-	fmt.Println(password, byte_password)
 
-	// GenerateFromPassword(password []byte, cost int) ([]byte, error)
+	// "hash" the registered password
 	hash_password, err := bcrypt.GenerateFromPassword(byte_password, bcrypt.MinCost)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(hash_password)
-	fmt.Println(string(hash_password))
+	// asks for "password" when logging in to the server
+	login_password := "12344"
+	byte_login_password := []byte(login_password)
+
+	// checks if the "entered password" (after hashing it) is equal to the registered "hashed" password
+	err = bcrypt.CompareHashAndPassword(hash_password, byte_login_password)
+	if err != nil {
+		fmt.Println("YOU CANNOT LOG IN\nerror:",err)
+		// function "main" terminates after executing the return statement
+		return
+	}
+	
+	fmt.Println("YOU HAVE SUCCESSFULLY LOGGED IN")
 }
