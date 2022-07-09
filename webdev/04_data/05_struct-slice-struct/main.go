45f3762230bbc2dc281e0fe18d47c8ff637e0b3f
lucasraal <lucasraal@gmail.com>
2022-07-09 00:19:49 -0300

    add data structures into templates

---
+++ webdev/04_data/05_struct-slice-struct/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type sage struct {
+	Name	string
+	Motto	string
+}
+
+type car struct {
+	Manufacturer	string
+	Model			string
+	Doors			int
+}
+
+type items struct {
+	Wisdom		[]sage
+	Transport	[]car
+}
+
+func init()  {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+	// tpl = template.Must(template.ParseFiles("tpl2.gohtml"))
+}
+
+func main()  {
+	b := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
+	}
+	g := sage{
+		Name:  "Gandhi",
+		Motto: "Be the change",
+	}
+	m := sage{
+		Name:  "Martin Luther King",
+		Motto: "Hatred never ceases with hatred but with love alone is healed.",
+	}
+
+	f := car{
+		Manufacturer: "Ford",
+		Model:        "F150",
+		Doors:        2,
+	}
+	c := car{
+		Manufacturer: "Toyota",
+		Model:        "Corolla",
+		Doors:        4,
+	}
+
+	sages := []sage{b, g, m}
+	cars := []car{f, c}
+
+	data := items{
+		Wisdom:		sages,
+		Transport:	cars,
+	}
+
+	// this could also have been done using an anonymous type (struct)
+	// data := struct {
+	// 	Wisdom    []sage
+	// 	Transport []car
+	// }{
+	// 	sages,
+	// 	cars,
+	// }
+
+	err := tpl.Execute(os.Stdout, data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
